f2f4e434d5a69a06b7782498dd250fe09b13a55f
Nathaliaaw <natalia4cardozo@gmail.com>
2021-06-20 17:19:53 -0500

    Implementacion de QR

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({Key? key}) : super(key: key);
@@ -7,8 +8,12 @@ class ScanButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       elevation: 0,
-      onPressed: null,
       child: Icon(Icons.filter_center_focus),
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+            '#3D8BEF', 'Cancelar', false, ScanMode.QR);
+        print(barcodeScanRes);
+      },
     );
   }
 }
